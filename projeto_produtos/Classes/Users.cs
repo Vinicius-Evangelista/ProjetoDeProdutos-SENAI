2189b8867e448674069dc29f94d6dc2b35cef71b
unknown <luccaslopes.n@gmail.com>
2021-06-09 23:30:24 -0300

    Interface e Classe (Users IUser) Feitas

---
+++ projeto_produtos/Classes/Users.cs
@@ -0,0 +1,40 @@
+using System;
+using projeto_produtos.Interfaces;
+namespace projeto_produtos.Classes
+{
+    public class Users
+    {
+        private int codigo { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        private DateTime DataCadastro;
+
+        public Users() { }
+
+        public Users(int _codigo, string _Nome, string _Email, string _Senha)
+        {
+            codigo = _codigo;
+            Nome = _Nome;
+            Email = _Email;
+            Senha = _Senha;
+            DataCadastro = DateTime.Now;
+        }
+
+
+        public string CadastrarUser(Users users)
+        {
+            return "Usuario cadastrado com sucesso !";
+        }
+
+        public string DeletarUser(Users users)
+        {
+
+            Nome = "";
+            Email = "";
+            Senha = "";
+
+            return "Usuario apagado";
+        }
+    }
+}
